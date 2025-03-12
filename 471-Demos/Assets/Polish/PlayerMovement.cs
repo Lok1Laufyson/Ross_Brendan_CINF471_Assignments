8726fccae1aa86d270277c5a715961de5892e66c
Lok1Laufyson <bross3@albany.edu>
2025-03-12 18:12:27 -0400

    save?

--- 471-Demos/Assets/Polish/PlayerMovement.cs
+++ 471-Demos/Assets/Polish/PlayerMovement.cs
@@ -1,24 +1,16 @@
 using UnityEngine;
 
-public class Treasure : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.GetComponent<PlayerMovement>())
-        {
-            //code will go here
-        }
+        
     }
 }
